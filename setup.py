7d0851d1e993859e793ca893c7783b3c8bf24463
longniao <longniao@gmail.com>
2018-11-21 09:17:32 +0800

    init

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(name='torstack',
+      version='0.0.1',
+      description='A full stack framework base on tornado.',
+      url='https://github.com/longniao/torstack',
+      author='longniao',
+      author_email='longniao@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ])
---
+++ torstack/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+def start():
+    print("hello torstack")
