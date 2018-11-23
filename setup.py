c2cd1eefae72e8a90cda58767234d6e83f5497be
he.pang <he.pang@yiducloud.cn>
2018-11-23 14:45:02 +0800

    setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,31 @@
 # -*- coding: utf-8 -*-
 
+import io
 from setuptools import setup, find_packages
+from collections import OrderedDict
 
-version = "0.0.1"
+version = "0.0.2"
+
+readme = ''
+with io.open('README.rst', 'rt', encoding='utf8') as f:
+    readme = f.read()
 
 setup(
     name='torstack',
     version=version,
     description='A full stack framework base on tornado.',
+    long_description=readme,
     url='https://github.com/longniao/torstack',
-    author='longniao',
+    project_urls=OrderedDict((
+        ('Documentation', 'https://github.com/longniao/torstack/tree/master/docs'),
+        ('Issue tracker', 'https://github.com/longniao/torstack/issues'),
+    )),
+    author='Longniao',
     author_email='longniao@gmail.com',
     license='MIT',
+    zip_safe=False,
+    platforms='any',
+    python_requires='>=3.5',
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
