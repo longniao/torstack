fa2bfdacecfbecd06e14a4cdec7eaa26ad9edfe5
longniao <longniao@gmail.com>
2018-12-26 22:22:32 +0800

    v0.0.5

--- publish.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-python setup.py sdist
-
-pip install dist/torstack-0.0.4.tar.gz
-
-# python setup.py sdist upload
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import io
 from setuptools import setup, find_packages
 from collections import OrderedDict
 
-version = "0.0.4"
+version = "0.0.5"
 
 readme = ''
 with io.open('README.rst', 'rt', encoding='utf8') as f:
