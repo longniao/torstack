37e3e23619d88cc4e7f01505c9383c109dca9dbf
longniao <longniao@gmail.com>
2018-12-26 22:19:11 +0800

    rm tests

--- tests/config/parser.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from torstack.config.parser import Parser
-
-config = Parser()
-config.load('/Users/panghe/Per/torstack/examples/__conf/dev.conf')
-print(config._dict)
-
--- tests/redis/server.py
+++
@@ -1,5 +0,0 @@
-import redis
-
-pool = redis.ConnectionPool(host='127.0.0.1',port=6379, password=None, db=2)
-client = redis.StrictRedis(connection_pool=pool, charset="utf-8", decode_responses=True)
-client.publish('channel', '{"a":"bbbccccc"}')
