ff0f21c25f8f246341dd3572f5fe6bf38abc9db8
longniao <longniao@gmail.com>
2019-02-21 13:40:52 +0800

    smtp service

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import io
 from setuptools import setup, find_packages
 from collections import OrderedDict
 
-version = "0.0.7"
+version = "0.0.8"
 
 readme = ''
 with io.open('README.rst', 'rt', encoding='utf8') as f:
--- torstack/smtp/listener.py
+++ torstack/smtp/listener.py
@@ -67,9 +67,12 @@ class SmtpListener(threading.Thread):
         '''
         if not self.client:
             try:
-                self.client = smtplib.SMTP(host=self.config.get('host'), port=self.config.get('port'), timeout=self.config.get('timeout'))
-                # self.client.set_debuglevel(True)
-                # self.client = smtplib.SMTP_SSL()
+                if self.config.get('use_ssl'):
+                    self.client = smtplib.SMTP_SSL(timeout=self.config.get('timeout'))
+                else:
+                    self.client = smtplib.SMTP(timeout=self.config.get('timeout'))
+                self.client.connect(host=self.config.get('host'), port=self.config.get('port'))
+                self.client.set_debuglevel(0)
                 self.client.login(self.config.get('username'), self.config.get('password'))
             except Exception as ex:
                 self.client = None
@@ -89,6 +92,18 @@ class SmtpListener(threading.Thread):
             message['To'] = formataddr([to_name, to_email])
             message['Subject'] = Header(subject, 'utf-8')
             self.client.sendmail(from_email, [to_email], message.as_string())
+        except smtplib.SMTPConnectError as e:
+            print('邮件发送失败，连接失败:', e.smtp_code, e.smtp_error)
+        except smtplib.SMTPAuthenticationError as e:
+            print('邮件发送失败，认证错误:', e.smtp_code, e.smtp_error)
+        except smtplib.SMTPSenderRefused as e:
+            print('邮件发送失败，发件人被拒绝:', e.smtp_code, e.smtp_error)
+        except smtplib.SMTPRecipientsRefused as e:
+            print('邮件发送失败，收件人被拒绝:', e.smtp_code, e.smtp_error)
+        except smtplib.SMTPDataError as e:
+            print('邮件发送失败，数据接收拒绝:', e.smtp_code, e.smtp_error)
+        except smtplib.SMTPException as e:
+            print('邮件发送失败, ', e.message)
         except Exception as ex:
             # SmtpManager.MAIL_LIST.append(email, [to_email], message.as_string())
             app_log.exception(ex)
